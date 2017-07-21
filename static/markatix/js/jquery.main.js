b2d1dcc572f589868fe145acb697db55c5499d3e
anup@123 <anupsamyal007@gmail.com>
2017-07-21 17:16:07 +0530

    navbar updated

--- static/markatix/js/jquery.main.js
+++ static/markatix/js/jquery.main.js
@@ -650,14 +650,14 @@ ResponsiveHelper = (function($){
 	};
 }(jQuery));
 
-// $(document).ready(function(){
-// 	$("#menu-close").click(function(e) {
-// 		$("#sidebar-wrapper").toggleClass("active");
-// 	});
-// 	$("#mt-main,article,.mt-logo,footer").click(function(e) {
-// 		$("#sidebar-wrapper").removeClass("active");
-// 	});
-// 	$("#menu-toggle").click(function(e) {
-// 		$("#sidebar-wrapper").toggleClass("active");
-// 	});
-// })
+$(document).ready(function(){
+	$("#menu-close").click(function(e) {
+		$("#sidebar-wrapper").toggleClass("active");
+	});
+	$("#mt-main,article,.mt-logo,footer").click(function(e) {
+		$("#sidebar-wrapper").removeClass("active");
+	});
+	$("#menu-toggle").click(function(e) {
+		$("#sidebar-wrapper").toggleClass("active");
+	});
+})
